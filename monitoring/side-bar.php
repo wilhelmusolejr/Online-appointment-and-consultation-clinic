417b11868698c106b884520b820f2e9b1090a04c
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2023-03-24 11:43:52 +0800

    fixed terms

--- monitoring/side-bar.php
+++ monitoring/side-bar.php
@@ -2,11 +2,11 @@
   <ul>
     <li class="pending-appointment <?php echo isset($pending) ? "active" :"" ?>"><a
         href="../pending-appointment/pending-appointment.php" class="text-uppercase">
-        <p>Pending monitor</p> <span class="number-notif flex-center">0</span>
+        <p>Pending monitoring</p> <span class="number-notif flex-center">0</span>
       </a></li>
     <li class="approved-appointment <?php echo isset($approved) ? "active" :"" ?>"><a
         href="../approved-appointment/approved-appointment.php" class="text-uppercase">
-        <p>Approved monitor</p>
+        <p>Approved monitoring</p>
       </a></li>
   </ul>
 </div>
