ae1d79901bef2f5711d321d7455d9e185e04e7d6
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2023-04-26 23:52:23 +0800

    fix link in gmail

--- php/config.php
+++ php/config.php
@@ -6,8 +6,8 @@
     $clientId = "660283226253-759r0jhhee768fa7hm0ums6db91gp1nv.apps.googleusercontent.com";
     $clientSecret = "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";
 
-    $link = "http://localhost/clinic";
-    // $link = "https://wmsu-dietitianconsult.online";
+    // $link = "http://localhost/clinic";
+    $link = "https://wmsu-dietitianconsult.online";
 
     $login_redirectUrl = $link."/php/request/req-login.php";
     $register_redirectUrl = $link."/php/set/set-register.php";
